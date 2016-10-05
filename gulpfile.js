a188af460fa9313330a1cd0b5eb7609e41ee1d47
Ian Scrivener <ian.scrivener@cabcharge.com.au>
2016-10-05 16:34:23 +1100

    Add Less processing, move bower to root

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
-var gulp = require('gulp');
-var clean = require('gulp-clean');
-var config = require('./config/config.json');
-var connect = require('gulp-connect');
-var gulpNgConfig = require('gulp-ng-config');
-var includeSources = require('gulp-include-source');
-var jade = require('gulp-jade');
-var minify = require('gulp-minify');
-var sass = require('gulp-ruby-sass');
+var gulp                = require('gulp');
+var clean               = require('gulp-clean');
+var config              = require('./config/config.json');
+var connect             = require('gulp-connect');
+var gulpNgConfig        = require('gulp-ng-config');
+var includeSources      = require('gulp-include-source');
+var jade                = require('gulp-jade');
+var minify              = require('gulp-minify');
+var sass                = require('gulp-ruby-sass');
+var less                = require('gulp-less');
+var path                = require('path');
 
-var devTasks = ['env-dev',   'libs', 'bootstrap-fonts', 'font-awesome-fonts', 'images', 'sass', 'jade', 'index', 'scripts', 'simple-server', 'watch'];
-var prodTasks = ['env-prod', 'libs', 'bootstrap-fonts', 'font-awesome-fonts', 'images', 'sass', 'jade', 'index', 'minify-scripts'];
+var devTasks = ['env-dev',   'libs', 'bootstrap-fonts', 'font-awesome-fonts', 'images', 'sass', 'less', 'jade', 'index', 'scripts', 'simple-server', 'watch'];
+var prodTasks = ['env-prod', 'libs', 'bootstrap-fonts', 'font-awesome-fonts', 'images', 'sass', 'less', 'jade', 'index', 'minify-scripts'];
 
 // set environment variable
 gulp.task('run-dev', devTasks);
@@ -30,6 +32,7 @@ gulp.task('env-dev', function () {
         }))
         .pipe(gulp.dest('dist/'))
 });
+
 gulp.task('env-prod', function () {
 
     gulp.src('config/env.json')
@@ -53,7 +56,6 @@ gulp.task('watch', function () {
 });
 
 gulp.task('libs', function () {
-
     gulp.src(config.path.js)
         .pipe(gulp.dest('dist/js'));
     gulp.src(config.path.css)
@@ -72,10 +74,17 @@ gulp.task('font-awesome-fonts', function () {
 });
 
 gulp.task('sass', function () {
-
     return sass(config.path.sass.src)
         .on('error', sass.logError)
         .pipe(gulp.dest(config.path.sass.dist));
+});
+
+gulp.task('less', function () {
+    return gulp.src(config.path.less.src)
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest(config.path.less.dist));
 });
 
 gulp.task('images', function () {
